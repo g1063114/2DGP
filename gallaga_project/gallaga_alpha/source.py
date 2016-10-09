aa3dda21af2a9521c687ab6bb230098de8a83c51
geonoh <ohhh123@naver.com>
2016-10-10 00:25:21 +0900

    add_resource

--- gallaga_project/gallaga_alpha/source.py
+++ gallaga_project/gallaga_alpha/source.py
@@ -2,7 +2,7 @@
 ############# GALLAGA_REMAKEver #################
 # Start project in 2016-10-02
 # 1. make code layout (2016-10-02)
-#
+# 2. add resource(player)
 # Made by Gunny
 #################################################
 
@@ -15,11 +15,16 @@ import math
 #airplane class (player)
 class airplane_player:
     def __init__(self):
-        x = 0
-        y = 0
-        self.image = load_image('player_airplane.png')
+        self.x, self.y = 0 , 10
+        self.frame = 0
+        self.image = load_image('player.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) & 7
+        self.x += 10
+
     def draw(self):
-        self.image.draw(x ,y)
+        self.image.clip_draw(self.frame*18, 10, 18, 18, self.x, self.y)
 
     pass
 
@@ -36,29 +41,39 @@ class airplane_enemy:
 
 def handle_events():
     #start var.
-    global launching
-
+    global running
 
     #define key
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
-            launching = False
+            running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            launching = False
+            running = False
 
 
 
 # initialization code
 open_canvas()
-
+running = True
+player = airplane_player()
+menu = 0
 
 
 # game main loop code
 
+#init screen
 
 
 
+#entering game
+while running:
+    handle_events()
+    player.update()
+
+    player.draw()
+    update_canvas()
+    delay(0.05)
 
 # finalization code
 close_canvas()

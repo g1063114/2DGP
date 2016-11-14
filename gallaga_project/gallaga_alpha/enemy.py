e3e2c29173d42d5629c1aeb4bdd16606d58d6061
geonoh <ohhh123@naver.com>
2016-11-15 04:10:03 +0900

    enemy.py added

--- gallaga_project/gallaga_alpha/enemy.py
+++ gallaga_project/gallaga_alpha/enemy.py
@@ -2,13 +2,49 @@ from pico2d import*
 import random
 
 class Enemy:
+    PIXEL_PER_METER = (10.0 / 0.3)           # 10 pixel 30 cm
+    RUN_SPEED_KMPH = 20.0                    # Km / Hour
+    RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+    RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+    RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+    TIME_PER_ACTION = 0.5
+    ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+    FRAMES_PER_ACTION = 8
+
+    image = None
+
+    LEFT_RUN, RIGHT_RUN, UP_RUN, DOWN_RUN, STAND = 0, 1, 2, 3, 4
+
     def __init__(self):
         self.x, self.y = 0, 0
-    def update(self, frash_time):
-        pass
+        #self.gen_x, self.gen_y = random.randint(-200,1000), random.randint(-100,700)
+        self.arr_x, self.arr_y = random.randint(1,20), random.randint(1,4)
+        self.dir_x = -1
+        self.dir_y = 0
+        self.image = load_image('enemyBlack.png')
+
+    def update(self, frame_time):
+        self.x = 800 - self.arr_x * 40
+        self.y = 600 - self.arr_y * 30
+        distance = Enemy.RUN_SPEED_PPS * frame_time
+        #self.y += (self.dir_y * distance)
+
+        #move left_right
+        #if self.x <= self.arr_x * 1:
+        #self.x += (self.dir_x * distance)
+        self.y += 20
+        #elif self.x > self.arr_x * 40:
+        #self.x -= (self.dir_x * distance)
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
     def get_bb(self):
-        pass
-    pass
+        return self.x - 20, self.y - 15, self.x + 20, self.y + 15
+
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
 
 
 #화면을 분할해서 배열에 넣어주고
--- gallaga_project/gallaga_alpha/main_state_test.py
+++ gallaga_project/gallaga_alpha/main_state_test.py
@@ -23,122 +23,54 @@ import os
 import game_framework
 import title_state
 from player import Player
+from enemy import Enemy
 
 
 name = "MainState"
 move_scale = 0.5
 player = None
+enemies = None
+back_ground = None
 
 # Game object class here
 def create_world():
-    global player
+    global player, enemies
     player = Player()
+    enemies = [Enemy() for i in range(40)]
+
 
 def destroy_world():
-    global player
+    global player, enemies
     del(player)
+    del(enemies)
+
 
 def enter():
     global back_ground
-    global air_enemy
-    global air_enemy2
-    global air_enemy3
-    global air_enemy4
-    global air_enemy5
-    global team
     back_ground = backGround()
 
-    #new added
+    # new added
     create_world()
 
-    team = [airplane_enemy() for i in range(20)]
-
-    for enemy in team:
-        enemy.x = random.randint(100, 700)
-        enemy.y = random.randint(100, 600)
-        pass
-
-
-    air_enemy = airplane_enemy()
-    air_enemy2 = airplane_enemy()
-    air_enemy3 = airplane_enemy()
-    air_enemy4 = airplane_enemy()
-    air_enemy5 = airplane_enemy()
-
-
-    air_enemy.ownX = 400
-    air_enemy.ownY = 300
-    air_enemy2.ownX = 440
-    air_enemy2.ownY = 300
-    air_enemy3.ownX = 480
-    air_enemy3.ownY = 300
-    air_enemy4.ownX = 520
-    air_enemy4.ownY = 300
-    air_enemy5.ownX = 560
-    air_enemy5.ownY = 300
 
 def exit():
-    global back_ground
-    global air_enemy
-    global air_enemy2
-    global air_enemy3
-    global air_enemy4
-    global air_enemy5
-    del(back_ground)
-    del(air_enemy)
-    del (air_enemy2)
-    del (air_enemy3)
-    del (air_enemy4)
-    del (air_enemy5)
-
-
-
     destroy_world()
 
-#airplane class (enemy)
-class airplane_enemy:
-    def __init__(self):
-        self.x, self.y = random.randint(-100, 800), random.randint(-100, 600)
-        self.ownX = random.randint(100, 700)
-        self.ownY = random.randint(300, 500)
-
-        #Add tile set.
-        #To deny overact
-        self.tileX = 0
-        self.tileY = 0
-        #self.ownX = 0
-        #self.ownY = 0
-        self.image = load_image('enemyBlack.png')
-
-    def update(self):
-        #enemy should find own place
-        if (self.x > self.ownX):
-            self.x -= move_scale
-        elif self.x < self.ownX:
-            self.x += move_scale
-        elif self.y > self.ownY:
-            self.y -= move_scale
-        elif self.y < self.ownY:
-            self.y += move_scale
-        pass
-    def draw(self):
-        self.image.draw(self.x, self.y)
-    pass
 
 class bullet:
     def __init__(self):
-
         pass
+
     def draw(self):
         pass
     pass
 
 
-#background image
+# background image
 class backGround:
     def __init__(self):
-        #not yet!
-        self.pagePoint = 3       #scroll the page
+        # not yet!
+        self.pagePoint = 3       # scroll the page
         self.pagePoint2 = 1
         self.image = load_image('background.png')
         self.image2 = load_image('background.png')
@@ -159,11 +91,6 @@ class backGround:
     pass
 
 
-
-
-
-
-
 def pause():
     pass
 
@@ -174,7 +101,7 @@ def resume():
 
 def handle_events(frame_time):
     events = get_events()
-    #start var.
+    # start var.
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
@@ -187,30 +114,26 @@ def handle_events(frame_time):
 
 def update(frame_time):
     player.update(frame_time)
+    for enemy in enemies:
+        enemy.update(frame_time)
     back_ground.update()
 
-    for air_enemyt in team:
-        air_enemyt.update()
-    pass
+
 
 
 def draw(frame_time):
     clear_canvas()
-    #don't change
+    # don't change
     back_ground.draw()
 
-    #start
-    #air_player.draw()
+    # start
+    # player.draw()
     player.draw()
     player.draw_bb()
 
-    for air_enemyt in team:
-        air_enemyt.draw()
+    # enemies
+    for enemy in enemies:
+        enemy.draw()
+        enemy.draw_bb()
 
-    air_enemy.draw()
-    air_enemy2.draw()
-    air_enemy3.draw()
-    air_enemy4.draw()
-    air_enemy5.draw()
     update_canvas()
-    pass

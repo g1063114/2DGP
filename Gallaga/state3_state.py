02e69e3e09cb30939d3422ac8cb0332a82fd1fc3
geonoh <ohhh123@naver.com>
2016-12-10 23:39:18 +0900

    state3_state.py added

---
+++ Gallaga/state3_state.py
@@ -0,0 +1,10 @@
+from pico2d import *
+import game_framework
+from player import Player
+from enemy import Enemy
+from bullet import Bullet, EnemyBullet
+import ranking_state
+from draw_score import ScoreDraw
+import main_state
+
+
